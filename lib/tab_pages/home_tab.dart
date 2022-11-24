c2519d837f310bd488c0eed6df5d43a62c6c1b73
faiyazsadi <sadi1807073@stud.kuet.ac.bd>
2022-11-24 16:33:47 +0600

    display map on app

--- lib/tab_pages/home_tab.dart
+++ lib/tab_pages/home_tab.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -8,10 +11,26 @@ class HomeTabPage extends StatefulWidget {
 }
 
 class _HomeTabPageState extends State<HomeTabPage> {
+  Completer<GoogleMapController> _controllerGooleMap = Completer();
+  GoogleMapController? newGoogleMapController;
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home'),
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controllerGooleMap.complete(controller);
+            newGoogleMapController = controller;
+          },
+        ),
+      ],
     );
   }
 }
