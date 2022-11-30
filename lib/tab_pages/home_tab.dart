c957c8ba5d476e0d5fc58c5bd67a0267e634f9fe
faiyazsadi <sadi1807073@stud.kuet.ac.bd>
2022-11-30 19:52:50 +0600

    added current user location

--- lib/tab_pages/home_tab.dart
+++ lib/tab_pages/home_tab.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_directions_api/google_directions_api.dart';
+import 'package:location/location.dart';
+import 'package:service_now/global/map_key.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({super.key});
@@ -13,21 +18,73 @@ class HomeTabPage extends StatefulWidget {
 class _HomeTabPageState extends State<HomeTabPage> {
   Completer<GoogleMapController> _controllerGooleMap = Completer();
   GoogleMapController? newGoogleMapController;
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+
+
+  static const LatLng sourceLocation = LatLng(37.4221, -122.0841);
+  static const LatLng destinationLocation = LatLng(37.4116, -122.0713);
+
+  List<LatLng> polyLineCoordinates = [];
+  LocationData? currentLocation;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+    await location.getLocation().then( 
+      (location) {
+        currentLocation = location;
+      },
+    );
+    //GoogleMapController googleMapController = await _controllerGooleMap.future;
+    //location.onLocationChanged.listen((newLocation) {
+      //currentLocation = newLocation;
+      //googleMapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(newLocation.latitude!, newLocation.longitude!),),),);
+      //setState(() {});
+    //},);
+  }
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      key, 
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destinationLocation.latitude, destinationLocation.longitude),
+    );
+    if(result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng pointLatLng) {
+          polyLineCoordinates.add(LatLng(pointLatLng.latitude, pointLatLng.longitude));
+      });
+      setState(() {});
+    }
+  }
+
+@override
+  void initState() {
+    // TODO: implement initState
+    getCurrentLocation();
+    sleep(const Duration(milliseconds: 200));
+    getPolyPoints();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        GoogleMap(
-          mapType: MapType.normal,
-          myLocationEnabled: true,
-          initialCameraPosition: _kGooglePlex,
-          onMapCreated: (GoogleMapController controller) {
-            _controllerGooleMap.complete(controller);
-            newGoogleMapController = controller;
+        currentLocation == null 
+        ?  const Center(child: Text("Loading..."),)
+        : GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),
+            zoom: 15.5,
+          ),
+          polylines: {
+            Polyline(
+              polylineId: PolylineId("route"),
+              points: polyLineCoordinates,
+              color: Colors.blueAccent,
+              width: 6,
+            )
+          },
+          markers: {
+            Marker(markerId: const MarkerId("currentLocation"), position: LatLng(currentLocation!.latitude!, currentLocation!.longitude!),),
           },
         ),
       ],
